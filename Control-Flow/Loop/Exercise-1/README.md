497df3f2a691d177bf6ac3d928a3b149d500b18c
thiago-cdb <fthiagocordeirodebrito@gmail.com>
2025-03-30 23:25:50 -0300

    Exercise 1 of loop structure completed.

---
+++ Control-Flow/Loop/Exercise-1/README.md
@@ -0,0 +1,10 @@
+# Even or Odd in a Range.
+
+## Write a program that receives the following value:
+* **A initial and a final number of a range**
+
+## The program must check:
+* **If each number in the range (including the first and last) is even or odd.**
+
+### The program should display:
+* **Each number and their classifcation**
---
+++ Control-Flow/Loop/Exercise-1/main.py
@@ -0,0 +1,23 @@
+i = 'y'
+
+while i.lower() == 'y':
+    initialNumber = int(input('Enter the first number of range: '))
+    finalNumber = int(input('Enter the last number of range: '))
+
+    step = 1 if initialNumber <= finalNumber else -1
+
+    for number in range(initialNumber, finalNumber + step, step):
+        result = 'even' if number % 2 == 0 else 'odd'
+        print(f'{number} is {result}')
+
+    i = input("Do you want to use the program again? (y/n): ").strip().lower()
+    
+print('Program terminated.')
+    
+
+    
+
+
+    
+    
+
